97414791cdef04cd898127b897c04db2fd30fc5d
Taylor Rubalcava <taylorboan@me.com>
2025-12-01 17:26:05 -0700

    Starting work on page scraper

---
+++ AutoTempestPageScraper.py
@@ -0,0 +1,12 @@
+TEST_URLs = [
+    "https://www.autotempest.com/trends/mnwq-f420edc5-7812-4373-b4ed-6982f153cda5?make=ferrari&model=all360&year_buckets=1999-2001%2C2002-2004#options",
+    "https://www.autotempest.com/trends/mvra-v1%7C267481488450%7C0?make=ferrari&model=all360&year_buckets=1999-2001,2002-2004",
+    "https://www.autotempest.com/trends/mnwq-cbd0d616-4e35-40b5-9189-e5ad00dcb608?make=ferrari&model=all360&year_buckets=1999-2001,2002-2004",
+    "https://www.autotempest.com/trends/mnwq-4e511389-d6ba-417f-bc96-11d52bf17b4c?make=ferrari&model=all360&year_buckets=1999-2001,2002-2004",
+    "https://www.autotempest.com/trends/mnwq-1a217908-9fc8-4358-bb1f-88d1e64d80b6?make=ferrari&model=all360&year_buckets=1999-2001,2002-2004",
+    "https://www.autotempest.com/trends/mvra-v1%7C396957288830%7C0?make=ferrari&model=all360&year_buckets=1999-2001,2002-2004",
+    "https://www.autotempest.com/trends/mnwq-41b6c4ec-3541-4d0d-8854-bca4c1309486?make=ferrari&model=all360&year_buckets=1999-2001,2002-2004",
+    "https://www.autotempest.com/trends/mnwq-27ca7bdd-68d8-4e13-884b-f9de40d1608b?make=ferrari&model=all360&year_buckets=1999-2001,2002-2004",
+    "https://www.autotempest.com/trends/mnwq-e6d28fe6-b7dd-4cf0-9b82-97cb38eb002d?make=ferrari&model=all360&year_buckets=1999-2001,2002-2004",
+    "https://www.autotempest.com/trends/mnwq-6dd49e81-5148-4b9a-b762-c44eea506544?make=ferrari&model=all360&year_buckets=1999-2001,2002-2004",
+]
